15150d9ef02c8ab5881c993dc3ae452970599d5d
Leo Tamminen <lettam@utu.fi>
2024-01-06 20:50:17 +0200

    first version

---
+++ password_hide.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main()
+{
+    system("cls");
+    char ch;
+    char pass[20], saved_pass[20];
+start:
+
+    printf("Enter your password : ");
+    int i = 0;
+    while (1)
+    {
+        ch = getch();
+        if(ch == 13)
+        {
+            pass[i]='\0';
+            break;
+        }
+        else
+            pass[i++] = ch;
+        printf("*");
+    }
+
+    printf("\nYou entered : %s\n ", pass);
+
+    FILE * ptr ;
+    ptr=fopen("password.txt","r");
+    fscanf(ptr, "%s", saved_pass);
+    fclose(ptr);
+
+    printf("Password %s\n ", saved_pass);
+
+    int l = strcmp(pass, saved_pass);
+
+    if(l == 0)
+    {
+        printf("\n\n******LOGIN SUCCESSFUL******\n");
+        
+    }
+    else
+    {
+        printf("\n\n******INCORRECT PASSWORD******\n");
+        goto start;
+    }
+
+    return 0;   
+}
